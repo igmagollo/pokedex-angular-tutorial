9a0a602120eda496b5bac81f6af942e85de0b32a
Igor Magollo <igraphmagollo@gmail.com>
2019-04-18 22:27:54 -0300

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'poke-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'pokedex-tutorial';
+}
